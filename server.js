10d5abfcaa49ffe98d71d0f27c3080b99f088e05
Melissa Deven <99757260+Deven1991@users.noreply.github.com>
2022-06-19 16:23:34 -0500

    added routes for API, HTML, and notes

--- server.js
+++ server.js
@@ -1,16 +1,34 @@
-// Dependencies
+//Dependencies
 const express = require('express');
 const uuid = require('uuid');
 const fs = require('fs');
 const path = require('path');
 
-// Express app
+//Express app
 const app = express();
 
-// Set port
-const PORT = process.env.PORT ||  3001;
+//Set port
+const PORT = process.env.PORT || 3001;
 
-// Express middleware
+//Express middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
+
+//API routes
+app.get('/api/notes', (req, res) => {
+    fs.readFile(path.join(__dirname, "./db/db.json"), 'utf8', (err, data) => {
+        if (err) throw err;
+        res.json(JSON.parse(data));
+    });
+});
+
+//HTML route
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+// Notes route
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
